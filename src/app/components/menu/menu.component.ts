aaadb83c0756c5efe6246f7c93c71608c8560913
Giancarlo <u202018645@upc.edu.pe>
2025-07-03 13:49:59 -0500

    Menu Correction

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 import { Router, RouterLink } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
   selector: 'app-menu',
-  imports: [MatIconModule,MatButtonModule,MatToolbarModule,MatMenuModule,RouterLink,CommonModule],
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    RouterLink,
+    CommonModule
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
-role: string = '';
-  constructor(private loginService: LoginService,private router:Router) {}
+  role: string = '';
+  menuDesplegado = false; // NUEVO: controla visibilidad del menú
+
+  constructor(private loginService: LoginService, private router: Router) {}
+
   cerrar() {
-    
     sessionStorage.clear();
     this.router.navigate(['/login']);
   }
@@ -27,11 +36,16 @@ role: string = '';
     this.role = this.loginService.showRole();
     return this.loginService.verificar();
   }
+
   isDeveloper() {
     return this.role === 'Developer';
   }
 
   isTester() {
     return this.role === 'Tester';
+  }
+
+  toggleMenu() {
+    this.menuDesplegado = !this.menuDesplegado;
   }
 }
